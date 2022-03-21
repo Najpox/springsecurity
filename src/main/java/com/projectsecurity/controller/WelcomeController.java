2dd80b3eee00cab4ee5f2076184e885f1c5169a9
MartinX3 <martin.horvath@innovis.sk>
2022-03-21 12:31:03 +0100

    log pridany

--- src/main/java/com/projectsecurity/controller/WelcomeController.java
+++ src/main/java/com/projectsecurity/controller/WelcomeController.java
@@ -1,8 +1,12 @@
 package com.projectsecurity.controller;
 
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
 import com.projectsecurity.config.TestBeans;
 import com.projectsecurity.model.Cards;
 import com.projectsecurity.repository.AccountTransactionsRepository;
+import org.apache.tomcat.util.buf.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -16,7 +20,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @RequestMapping("/apiw")
 public class WelcomeController {
 
-   @Autowired
+    private Logger log = LoggerFactory.getLogger(WelcomeController.class);
+
+    @Autowired
     private testService testService;
 
     @GetMapping("/welcome")
@@ -26,6 +32,7 @@ public class WelcomeController {
         //bean sa vykona iba ak nad testbeans je configuration anotacia
         //System.out.println("pringtinng11x " + testService.getList().get(0).getTotalLimit());
 
+        log.info("testtt loog");
         ApplicationContext apcntx = new AnnotationConfigApplicationContext(TestBeans.class);
         testService serviceBeans =  apcntx.getBean(testService.class);
         System.out.println("pringtinngssssssssssss " + serviceBeans);
